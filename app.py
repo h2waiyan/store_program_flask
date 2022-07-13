e02d27cd7f0ad1ee7fdfb1d9b582bd90166ea331
Htoo Wai Yan <h2wy.mog@gmail.com>
2022-07-13 10:04:06 +0630

    Create Tables

--- app.py
+++ app.py
@@ -23,7 +23,13 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
