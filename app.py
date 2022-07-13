c2307e2cc610ad54a2e7b5d088fc862dad9fa765
Htoo Wai Yan <h2wy.mog@gmail.com>
2022-07-13 10:27:13 +0630

    fix bugs

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):     
     uri = uri.replace("postgres://", "postgresql+psycopg2://", 1)
 
-print(uri)
+print(">>>>>>" + uri)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri, 'sqlite:///mydatabase.db'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 
-app.config['DEBUG'] = True
+# app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "mont-hin-gha-and-pizza"
 api = Api(app)
@@ -37,9 +37,9 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
+    # if app.config['DEBUG']:
+    #     @app.before_first_request
+    #     def create_tables():
+    #         db.create_all()
 
     app.run(port=5000)
