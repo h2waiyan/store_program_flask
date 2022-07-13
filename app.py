4832adc1c6b8a7477426f9f50b8cdc970b310951
Htoo Wai Yan <h2wy.mog@gmail.com>
2022-07-13 09:47:48 +0630

    Postgres Added

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +8,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///mydatabase.db')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "mont-hin-gha-and-pizza"
