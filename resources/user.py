05a77ba25383ed309831d74563aec5594d3a2cce
Htoo Wai Yan <95065183+h2waiyan@users.noreply.github.com>
2022-07-18 18:49:13 +0630

    Update user.py

--- resources/user.py
+++ resources/user.py
@@ -26,3 +26,7 @@ class UserRegister(Resource):
         user.save_to_db()
 
         return { "message" : "User created successfully."}, 201
+
+class Home(Resource):
+    def get(self):
+        return { "message" : "Your app is working" }
